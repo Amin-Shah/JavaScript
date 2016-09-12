07a56e26dcb6d7484f5731e15d2ba1119591849a
Ameen Shah <Ameen Shah>
2016-09-12 07:35:11 -0700

    JavaScript Practice

---
+++ app.js
@@ -0,0 +1,244 @@
+// var spec = prompt("Your Species");
+// alert(spec);
+
+
+// var name = prompt("What is your Name : ");
+// alert(name + " Shah");
+
+
+// var name1 = prompt("Name", "What is your Name : ");
+// alert(name1 + "Amin Shah");
+
+
+
+// Using Prompt Two Values Addition
+
+// var val1 = prompt("Enter a First value");
+// var val2 = prompt("Enter a Second value");
+// alert(parseInt(val1) + parseInt(val2));
+
+
+//MarkSheet
+
+// var mark = prompt("Enter Your Marks");
+// if (mark == 90) {
+//     alert("A+ Grade");
+// } 
+// if (mark == 80) {
+//     alert("A1 Grade");
+// }
+// if (mark == 70) {
+//     alert("A Grade");
+// }
+// if (mark == 60) {
+//     alert("B Grade");
+// }
+// if (mark == 50) {
+//     alert("C Grade");
+// }
+// if (mark == 40) {
+//     alert("D Grade");
+// }
+
+
+//Check Percentage
+
+// var mark1 = prompt("Enter Your Percentage");
+
+// if (mark1 == 20) {
+//     alert("Percetage" +"  "+ 20/20*100 +'%,' +  "         "  + "Grade A+");
+// }
+// if (mark1 == 19) {
+//     alert("Percetage" +"  "+ 19/20*100 + '%,' + "        "  + "Grade A+");
+// } 
+// if (mark1 == 18) {
+//     alert("Percetage" +"  "+ 18/20*100 + '%,' + "        "  + "Grade A1");
+// } 
+// if (mark1 == 17) {
+//     alert("Percetage" +"  "+ 17/20*100 + '%,' + "        "  + "Grade A1");
+// } 
+// if (mark1 == 16) {
+//     alert("Percetage" +"  "+ 16/20*100 + '%,' + "        "  + "Grade A");
+// } 
+// if (mark1 == 15) {
+//     alert("Percetage" +"  "+ 15/20*100 + '%,' + "        "  + "Grade A");
+// } 
+// if (mark1 == 14) {
+//     alert("Percetage" +"  "+ 14/20*100 + '%,' + "        "  + "Grade B");
+// } 
+// if (mark1 == 13) {
+//     alert("Percetage" +"  "+ 13/20*100 + '%,' + "        "  + "Grade B");
+// } 
+// if (mark1 == 12) {
+//     alert("Percetage" +"  "+ 12/20*100 + '%,' + "        "  + "Grade C");
+// } 
+// if (mark1 == 11) {
+//     alert("Percetage" +"  "+ 11/20*100 + '%,' + "        "  + "Grade C");
+// } 
+// if (mark1 == 10) {
+//     alert("Percetage" +"  "+ 10/20*100 + '%,' + "        "  + "Grade D");
+// } 
+// if (mark1 == 9) {
+//     alert("Percetage" +"  "+ 9/20*100 + '%,' + "        "  + "Grade D");
+// } 
+// if (mark1 == 8) {
+//     alert("Percetage" +"  "+ 8/20*100 + '%,' + "        "  + "Grade E");
+// } 
+// if (mark1 == 7) {
+//     alert("Percetage" +"  "+ 7/20*100 + '%,' + "        "  + "Grade E");
+// } 
+// if (mark1 == 6) {
+//     alert("Percetage" +"  "+ 6/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 5) {
+//     alert("Percetage" +"  "+ 5/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 4) {
+//     alert("Percetage" +"  "+ 4/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 3) {
+//     alert("Percetage" +"  "+ 3/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 2) {
+//     alert("Percetage" +"  "+ 2/20*100 + '%,' + "        "  + "Grade -");
+// } 
+// if (mark1 == 1) {
+//     alert("Percetage" +"  "+ 1/20*100 + '%,' + "        "  + "Grade -");
+// } 
+
+
+//Search by ID and Name
+
+
+// var Id = prompt("Enter Your ID");
+// var Name = prompt("Enter Your Name");
+
+// if (Id == 1 && Name == 'Ehsan') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 20 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ +20/20*100 +'%\n' + "Grade A+");
+// }
+// if (Id == 2 && Name == 'Sami') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 19 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 19/20*100 +'%\n' + "Grade A+");
+// }
+// if (Id == 3 && Name == 'Amin') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 18 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 18/20*100 +'%\n' + "Grade A");
+// }
+// if (Id == 4 && Name == 'Faisal') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 17 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 17/20*100 +'%\n' + "Grade A");
+// }
+// if (Id == 5 && Name == 'Saad') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 16 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 16/20*100 +'%\n' + "Grade B");
+// }
+// if (Id == 6 && Name == 'Ahmed') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 15 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 15/20*100 +'%\n' + "Grade B");
+// }
+// if (Id == 7 && Name == 'Hameed') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 14 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 14/20*100 +'%\n' + "Grade C");
+// }
+// if (Id == 8 && Name == 'Ali') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 13 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ +13/20*100 +'%\n' + "Grade C");
+// }
+// if (Id == 9 && Name == 'Hasan') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 12 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ +12/20*100 +'%\n' + "Grade D");
+// }
+// if (Id == 10 && Name == 'Waqas') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 11 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 11/20*100 +'%\n' + "Grade D");
+// }
+// if (Id == 11 && Name == 'Bilal') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 10 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 10/20*100 +'%\n' + "Grade E");
+// }
+// if (Id == 12 && Name == 'Ahsan') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 9 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 9/20*100 +'%\n' + "Grade E");
+// }
+// if (Id == 13 && Name == 'Noman') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 8 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 8/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 14 && Name == 'Amir') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 7 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 7/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 15 && Name == 'Ejaz') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 6 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 6/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 16 && Name == 'Khalil') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 5 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 5/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 17 && Name == 'Husnain') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 4 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 4/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 18 && Name == 'Khuzair') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 3 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 3/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 19 && Name == 'Mahmood') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 2 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 2/20*100 +'%\n' + "Grade -");
+// }
+// if (Id == 19 && Name == 'Mohsin') {
+//     alert("ID : " + Id + '\n' + "Name : " + Name + '\n' +"Obtained Marks = 1 \n"+ "Total Marks  = 20 \n" +"Percentage : "+ 1/20*100 +'%\n' + "Grade -");
+// }
+
+
+
+// Search by ID
+
+var Id = prompt("Enter Your ID");
+
+if (Id == 1) {
+    alert("ID : " + Id + '\n' + "Name : " + "Ehsan" + '\n' + "Obtained Marks = 20 \n" + "Total Marks  = 20 \n" + "Percentage : " + +20 / 20 * 100 + '%\n' + "Grade A+");
+}
+if (Id == 2) {
+    alert("ID : " + Id + '\n' + "Name : " + "Sami" + '\n' + "Obtained Marks = 19 \n" + "Total Marks  = 20 \n" + "Percentage : " + 19 / 20 * 100 + '%\n' + "Grade A+");
+}
+if (Id == 3) {
+    alert("ID : " + Id + '\n' + "Name : " + "Amin" + '\n' + "Obtained Marks = 18 \n" + "Total Marks  = 20 \n" + "Percentage : " + 18 / 20 * 100 + '%\n' + "Grade A");
+}
+if (Id == 4) {
+    alert("ID : " + Id + '\n' + "Name : " + "Faisal" + '\n' + "Obtained Marks = 17 \n" + "Total Marks  = 20 \n" + "Percentage : " + 17 / 20 * 100 + '%\n' + "Grade A");
+}
+if (Id == 5) {
+    alert("ID : " + Id + '\n' + "Name : " + "Saad" + '\n' + "Obtained Marks = 16 \n" + "Total Marks  = 20 \n" + "Percentage : " + 16 / 20 * 100 + '%\n' + "Grade B");
+}
+if (Id == 6) {
+    alert("ID : " + Id + '\n' + "Name : " + "Ahmed" + '\n' + "Obtained Marks = 15 \n" + "Total Marks  = 20 \n" + "Percentage : " + 15 / 20 * 100 + '%\n' + "Grade B");
+}
+if (Id == 7) {
+    alert("ID : " + Id + '\n' + "Name : " + "Hameed" + '\n' + "Obtained Marks = 14 \n" + "Total Marks  = 20 \n" + "Percentage : " + 14 / 20 * 100 + '%\n' + "Grade C");
+}
+if (Id == 8) {
+    alert("ID : " + Id + '\n' + "Name : " + "Ali" + '\n' + "Obtained Marks = 13 \n" + "Total Marks  = 20 \n" + "Percentage : " + +13 / 20 * 100 + '%\n' + "Grade C");
+}
+if (Id == 9) {
+    alert("ID : " + Id + '\n' + "Name : " + "Hasan" + '\n' + "Obtained Marks = 12 \n" + "Total Marks  = 20 \n" + "Percentage : " + +12 / 20 * 100 + '%\n' + "Grade D");
+}
+if (Id == 10) {
+    alert("ID : " + Id + '\n' + "Name : " + "Waqas" + '\n' + "Obtained Marks = 11 \n" + "Total Marks  = 20 \n" + "Percentage : " + 11 / 20 * 100 + '%\n' + "Grade D");
+}
+if (Id == 11) {
+    alert("ID : " + Id + '\n' + "Name : " + "Bilal" + '\n' + "Obtained Marks = 10 \n" + "Total Marks  = 20 \n" + "Percentage : " + 10 / 20 * 100 + '%\n' + "Grade E");
+}
+if (Id == 12) {
+    alert("ID : " + Id + '\n' + "Name : " + "Ahsan" + '\n' + "Obtained Marks = 9 \n" + "Total Marks  = 20 \n" + "Percentage : " + 9 / 20 * 100 + '%\n' + "Grade E");
+}
+if (Id == 13) {
+    alert("ID : " + Id + '\n' + "Name : " + "Noman" + '\n' + "Obtained Marks = 8 \n" + "Total Marks  = 20 \n" + "Percentage : " + 8 / 20 * 100 + '%\n' + "Grade -");
+}
+if (Id == 14) {
+    alert("ID : " + Id + '\n' + "Name : " + "Amir" + '\n' + "Obtained Marks = 7 \n" + "Total Marks  = 20 \n" + "Percentage : " + 7 / 20 * 100 + '%\n' + "Grade -");
+}
+if (Id == 15) {
+    alert("ID : " + Id + '\n' + "Name : " + "Ejaz" + '\n' + "Obtained Marks = 6 \n" + "Total Marks  = 20 \n" + "Percentage : " + 6 / 20 * 100 + '%\n' + "Grade -");
+}
+if (Id == 16) {
+    alert("ID : " + Id + '\n' + "Name : " + "Khalil" + '\n' + "Obtained Marks = 5 \n" + "Total Marks  = 20 \n" + "Percentage : " + 5 / 20 * 100 + '%\n' + "Grade -");
+}
+if (Id == 17) {
+    alert("ID : " + Id + '\n' + "Name : " + "Husnain" + '\n' + "Obtained Marks = 4 \n" + "Total Marks  = 20 \n" + "Percentage : " + 4 / 20 * 100 + '%\n' + "Grade -");
+}
+if (Id == 18) {
+    alert("ID : " + Id + '\n' + "Name : " + "Khuzair" + '\n' + "Obtained Marks = 3 \n" + "Total Marks  = 20 \n" + "Percentage : " + 3 / 20 * 100 + '%\n' + "Grade -");
+}
+if (Id == 19) {
+    alert("ID : " + Id + '\n' + "Name : " + "Mahmood" + '\n' + "Obtained Marks = 2 \n" + "Total Marks  = 20 \n" + "Percentage : " + 2 / 20 * 100 + '%\n' + "Grade -");
+}
+if (Id == 20) {
+    alert("ID : " + Id + '\n' + "Name : " + "Abid" + '\n' + "Obtained Marks = 1 \n" + "Total Marks  = 20 \n" + "Percentage : " + 1 / 20 * 100 + '%\n' + "Grade -");
+} else {
+    alert("Doesn't Match any ID");
+}
